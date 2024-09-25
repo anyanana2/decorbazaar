23445075e7263f848e2ee0d70972d631f3d4b6ef
HydraCat <roan.h.gibbons@gmail.com>
2024-09-25 06:32:37 +0100

    Added config system with configs for all currently customised decay rates, added patch to insert the config file values into the asset configurations at runtime

---
+++ DecorBazaar/src/DecorBazaarConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.GameContent;
+using Vintagestory.ServerMods.NoObf;
+
+namespace DecorBazaar
+{
+    internal class DecorBazaarConfig
+    {
+        public static DecorBazaarConfig Current { get; set; }
+
+        public float vegstorageVegetableDecay = 0.5f;
+        public float vegstorageGrainDecay = 0.75f;
+        public float breadboxVegetableDecay = 0.75f;
+        public float breadboxGrainDecay = 0.5f;
+        public float mushroomstorageVegetableDecay = 0.5f;
+        public float mushroomstorageGrainDecay = 0.5f;
+        public float meatstorageProteinDecay = 0.5f;
+        public float compostbinVegetableDecay = 2.0f;
+        public float compostbinGrainDecay = 2.0f;
+
+        public static DecorBazaarConfig GetDefault()
+        {
+            DecorBazaarConfig config =  new DecorBazaarConfig();
+            config.vegstorageVegetableDecay.ToString();
+            config.vegstorageGrainDecay.ToString();
+            config.breadboxVegetableDecay.ToString();
+            config.breadboxGrainDecay.ToString();
+            config.mushroomstorageVegetableDecay.ToString();
+            config.mushroomstorageGrainDecay.ToString();
+            config.meatstorageProteinDecay.ToString();
+            config.compostbinVegetableDecay.ToString();
+            config.compostbinGrainDecay.ToString();
+            return config;
+        }
+    }
+}
--- DecorBazaar/src/DecorBazaarModSystem.cs
+++ DecorBazaar/src/DecorBazaarModSystem.cs
@@ -1,17 +1,64 @@
-﻿using Vintagestory.API.Client;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
+using Vintagestory.Common;
+using Vintagestory.ServerMods.NoObf;
+using Vintagestory.API.Datastructures;
 
 namespace DecorBazaar
 {
     public class DecorBazaarModSystem : ModSystem
     {
-
+        ICoreAPI api;
         public override void Start(ICoreAPI api)
         {
+            this.api = api;
+            loadConfig();            
+            
             api.RegisterBlockEntityClass("DecorBazaarStove", typeof(BlockEntityDecorStove));
         }
 
+        public void loadConfig()
+        {
+            try
+            {
+                DecorBazaarConfig DecorBazaarConfig = api.LoadModConfig<DecorBazaarConfig>("DecorBazaar.json");
+                if (DecorBazaarConfig != null)
+                {
+                    api.Logger.Notification("Mod Config successfully loaded.");
+                    DecorBazaarConfig.Current = DecorBazaarConfig;
+                }
+                else
+                {
+                    api.Logger.Notification("No Mod Config specified. Falling back to default settings");
+                    DecorBazaarConfig.Current = DecorBazaarConfig.GetDefault();
+                }
+            }
+            catch
+            {
+                DecorBazaarConfig.Current = DecorBazaarConfig.GetDefault();
+                api.Logger.Error("Failed to load custom mod configuration. Falling back to default settings!");
+            }
+            finally
+            {
+                api.StoreModConfig<DecorBazaarConfig>(DecorBazaarConfig.Current, "DecorBazaar.json");
+            }
+            setConfig();
+        }
+        public void setConfig()
+        {
+            api.World.Config.SetFloat("vegstorageVegetableDecay", DecorBazaarConfig.Current.vegstorageVegetableDecay);
+            api.World.Config.SetFloat("vegstorageGrainDecay", DecorBazaarConfig.Current.vegstorageGrainDecay);
+            api.World.Config.SetFloat("breadboxVegetableDecay", DecorBazaarConfig.Current.breadboxVegetableDecay);
+            api.World.Config.SetFloat("breadboxGrainDecay", DecorBazaarConfig.Current.breadboxGrainDecay);
+            api.World.Config.SetFloat("mushroomstorageVegetableDecay", DecorBazaarConfig.Current.mushroomstorageVegetableDecay);
+            api.World.Config.SetFloat("mushroomstorageGrainDecay", DecorBazaarConfig.Current.mushroomstorageGrainDecay);
+            api.World.Config.SetFloat("meatstorageProteinDecay", DecorBazaarConfig.Current.meatstorageProteinDecay);
+            api.World.Config.SetFloat("compostbinVegetableDecay", DecorBazaarConfig.Current.compostbinVegetableDecay);
+            api.World.Config.SetFloat("compostbinGrainDecay", DecorBazaarConfig.Current.compostbinGrainDecay);
+        }
     }
 }
