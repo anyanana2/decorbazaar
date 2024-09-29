f70c50a10fb8dfca998957abd9a185bf50f5d0e8
HydraCat <roan.h.gibbons@gmail.com>
2024-09-29 13:56:41 +0100

    Simplified the process of adding a new configurable to adding to a dict, made robust to new configurables when the config file already exists

--- DecorBazaar/src/DecorBazaarModSystem.cs
+++ DecorBazaar/src/DecorBazaarModSystem.cs
@@ -16,49 +16,11 @@ namespace DecorBazaar
         public override void Start(ICoreAPI api)
         {
             this.api = api;
-            loadConfig();            
+            DecorBazaarConfig.loadConfig(api);
             
             api.RegisterBlockEntityClass("DecorBazaarStove", typeof(BlockEntityDecorStove));
         }
 
-        public void loadConfig()
-        {
-            try
-            {
-                DecorBazaarConfig DecorBazaarConfig = api.LoadModConfig<DecorBazaarConfig>("DecorBazaar.json");
-                if (DecorBazaarConfig != null)
-                {
-                    api.Logger.Notification("Mod Config successfully loaded.");
-                    DecorBazaarConfig.Current = DecorBazaarConfig;
-                }
-                else
-                {
-                    api.Logger.Notification("No Mod Config specified. Falling back to default settings");
-                    DecorBazaarConfig.Current = DecorBazaarConfig.GetDefault();
-                }
-            }
-            catch
-            {
-                DecorBazaarConfig.Current = DecorBazaarConfig.GetDefault();
-                api.Logger.Error("Failed to load custom mod configuration. Falling back to default settings!");
-            }
-            finally
-            {
-                api.StoreModConfig<DecorBazaarConfig>(DecorBazaarConfig.Current, "DecorBazaar.json");
-            }
-            setConfig();
-        }
-        public void setConfig()
-        {
-            api.World.Config.SetFloat("vegstorageVegetableDecay", DecorBazaarConfig.Current.vegstorageVegetableDecay);
-            api.World.Config.SetFloat("vegstorageGrainDecay", DecorBazaarConfig.Current.vegstorageGrainDecay);
-            api.World.Config.SetFloat("breadboxVegetableDecay", DecorBazaarConfig.Current.breadboxVegetableDecay);
-            api.World.Config.SetFloat("breadboxGrainDecay", DecorBazaarConfig.Current.breadboxGrainDecay);
-            api.World.Config.SetFloat("mushroomstorageVegetableDecay", DecorBazaarConfig.Current.mushroomstorageVegetableDecay);
-            api.World.Config.SetFloat("mushroomstorageGrainDecay", DecorBazaarConfig.Current.mushroomstorageGrainDecay);
-            api.World.Config.SetFloat("meatstorageProteinDecay", DecorBazaarConfig.Current.meatstorageProteinDecay);
-            api.World.Config.SetFloat("compostbinVegetableDecay", DecorBazaarConfig.Current.compostbinVegetableDecay);
-            api.World.Config.SetFloat("compostbinGrainDecay", DecorBazaarConfig.Current.compostbinGrainDecay);
-        }
+        
     }
 }
